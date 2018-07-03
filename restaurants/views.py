258e63919a984437b8ac7975d1add56b0d95d5fa
MAC <mac@Ibrahiim-IK.local>
2018-07-03 18:40:13 +0300

    finished task3

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render
 
 def welcome(request):
-    return render(request, 'index.html', {'msg':'Hello World!'})
+	return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
 
-    context = {
+	context = {
+		"my_list":
+			[
+				{
+					"restaurant_name":"burgerhub",
+					"food_type":"burgers",
+				},
+				{
+					"restaurant_name":"pizzahub",
+					"food_type":"pizzas",
+				},
+				{
+					"restaurant_name":"pastahub",
+					"food_type":"pastas",
+				},
+			]
 
-    }
-    return render(request, 'list.html', context)
+		}
+	return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
-    context = {
+	context = {
+			"my_object":
+				{
+					"restaurant_name":"pastahub",
+					"food_type":"pastas",
+				}
 
-    }
-    return render(request, 'detail.html', context)
+	}
+	return render(request, 'detail.html', context)
